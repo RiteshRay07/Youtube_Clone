aaee597f1e15281a3f058b9c16ed25484679239c
riteshray07 <riteshray657080@gmail.com>
2022-09-29 07:28:24 +0530

    int. commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+// -------------- Side-bar start -----------
+
+
+var menuIcon = document.querySelector(".menu-icon");
+var sideBar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+
+menuIcon.onclick = function(){
+    sideBar.classList.toggle("small-sidebar"); 
+    container.classList.toggle("large-container"); 
+}
+
+
+
+
+// ----------------- Side-bar End --------------
+
